742e824c02ce6fe0d02ddcc3a60156649484590f
LeaaJr <leeitooocabj@gmail.com>
2025-02-07 17:53:42 +0100

    add-header

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "./Components/Navbar";
+
+const App = () => {
+  const [productos, setProductos] = useState([]);
+
+  // Función para obtener los productos
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/products")  // Asegúrate de que la URL coincida con tu backend
+      .then((response) => {
+        setProductos(response.data);  // Asignamos los productos a nuestro estado
+      })
+      .catch((error) => {
+        console.error("Error al obtener los productos:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+
+      <Navbar />
+
+{/*       <h1>Lista de Productos</h1>
+      <ul>
+        {productos.map((producto) => (
+          <li key={producto.id}>
+            {producto.nombre} - ${producto.precio}
+          </li>
+        ))}
+      </ul> */}
+    </div>
+  );
+};
+
+export default App;
