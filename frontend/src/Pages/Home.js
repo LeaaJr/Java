21b5c9afc180b55d5a29c770fd181d936b6e4455
LeaaJr <leeitooocabj@gmail.com>
2025-02-11 22:33:46 +0100

    add-cards

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Header } from "../Components/Header";
+import SectionCard from "../Sections/SectionCards";
 
 export function Home() {
 
-  return <Header />;
+  return ( 
+          <>
+          <Header />;
+          <SectionCard />;
+
+          </>
+  )
 }
 
 export default Home;
